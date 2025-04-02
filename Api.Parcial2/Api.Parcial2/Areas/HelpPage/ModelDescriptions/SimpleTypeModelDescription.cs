e197c86dd7074fef4a1d66c158f482e89ca2a84b
Estiben Montoya Taborda - Analista TI <emontoya@cidet.org.co>
2025-04-02 06:59:39 -0500

    proyecto parcial 1

---
+++ Api.Parcial2/Api.Parcial2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Api.Parcial2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Api.Parcial2/Api.Parcial2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Api.Parcial2.WebApiApplication" Language="C#" %>
